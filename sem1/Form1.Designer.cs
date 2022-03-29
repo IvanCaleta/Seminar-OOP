7e70cbb6a2b0577ec318474347532fce51ea53b2
ivan <icalet00@fesb.hr>
2022-03-30 00:22:40 +0200

    dodavanje

---
+++ sem1/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace sem1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(78, 81);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(132, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HRK->";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "EUR",
+            "USD",
+            "RSD",
+            "KM",
+            "DEN",
+            "GBP"});
+            this.comboBox1.Location = new System.Drawing.Point(399, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(117, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(261, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(210, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Izlaz";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(516, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "?";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "?";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(210, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 31);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Spremi";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(353, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "?";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(210, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 32);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Ucitaj";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(353, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "?";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 477);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ sem1/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+namespace sem1
+{
+    public partial class Form1 : Form
+    {
+        StreamWriter sw = new StreamWriter("E:/seminar/sem1/sem1/buffer.txt");
+        StreamReader sr = new StreamReader("E:/seminar/sem1/sem1/ucitaj.txt");
+        public Form1()
+        {
+            sw.Write("Rezultati:");
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.comboBox1.SelectedItem = "EUR";
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Minimum = 0;
+
+            numericUpDown1.Maximum = 25000;
+
+            pretvaraj();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pretvaraj();
+        }
+        private void pretvaraj()
+        {
+            decimal broj = this.numericUpDown1.Value;
+            string valuta = this.comboBox1.SelectedItem.ToString();
+            decimal novo = broj;
+
+            if (valuta == "EUR")
+                novo = broj / 7.56m;
+            else if (valuta == "USD")
+                novo = broj / 6.13m;
+            else if (valuta == "RSD")
+                novo = broj * 15.55m;
+            else if (valuta == "KM")
+                novo = broj / 3.88m;
+            else if (valuta == "DEN")
+                novo = broj * 8.14m;
+            else if (valuta == "GBP")
+                novo = broj / 8.35m;
+
+
+            novo = decimal.Round(novo,4,MidpointRounding.AwayFromZero);
+
+            this.label2.Text = novo + " " + valuta;
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Jeste li sigurni?", "Potvrdi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                sw.Write("\nKraj!");
+                sr.Close();
+                sw.Close();
+
+                this.Close();
+            }
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            DialogResult f = MessageBox.Show("Ovdje birate u koju valutu želite pretvoriti kune","Pomoć",MessageBoxButtons.OK);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            DialogResult d = MessageBox.Show("Ovdje unosite određeni iznos u kunama.\nMaksimum je 25000.", "Pomoć", MessageBoxButtons.OK);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string str = this.label2.Text;
+            sw.Write("\n");
+            sw.Write(str);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str = sr.ReadLine();
+            decimal br = System.Convert.ToDecimal(str);
+            this.numericUpDown1.Value = br;
+            
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            DialogResult d = MessageBox.Show("Klikom na Ucitaj se ucitava iznos iz mape ucitaj.txt", "Pomoć", MessageBoxButtons.OK);
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            DialogResult d = MessageBox.Show("Klikom na Spremi se spremaju rezultati u mapu buffer.txt\n", "Pomoć", MessageBoxButtons.OK);
+
+        }
+    }
+}
